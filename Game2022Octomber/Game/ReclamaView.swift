c1397820f1476d9e2d8906258155ba814adfcaa3
Ivan Dimitrov <biobeehive@abv.bg>
2022-10-13 02:11:01 +0300

    Sound

---
+++ Game2022Octomber/Game/ReclamaView.swift
@@ -0,0 +1,23 @@
+//
+//  ReclamaView.swift
+//  Game2022Octomber
+//
+//  Created by Dosi Dimitrov on 13.10.22.
+//
+
+import SwiftUI
+
+struct ReclamaView: View {
+    var body: some View {
+        ZStack {
+            Color.purple
+            Text("reclama")
+        }
+    }
+}
+
+struct ReclamaView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReclamaView()
+    }
+}
