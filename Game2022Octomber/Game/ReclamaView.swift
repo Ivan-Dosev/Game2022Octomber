2e71fb4256e8d3cf70707c11ee0ff19bedd0d85b
Ivan Dimitrov <biobeehive@abv.bg>
2022-10-15 09:21:47 +0300

    Reset score if not Reclama

--- Game2022Octomber/Game/ReclamaView.swift
+++ Game2022Octomber/Game/ReclamaView.swift
@@ -8,10 +8,55 @@
 import SwiftUI
 
 struct ReclamaView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject               var gameSetting : GameSetting
+    
     var body: some View {
         ZStack {
-            Color.purple
-            Text("reclama")
+            Image("BGReclama")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .rotationEffect(.degrees(90))
+                .opacity(0.5)
+            qestionShape()
+                .fill(Color.red)
+            //   .stroke(Color.yellow, lineWidth: 1)
+               .frame(width: 350, height: 350)
+               .shadow(color: Color.white, radius: 20, x: -5, y: -5)
+               .overlay(
+                VStack(alignment: .center, spacing: 50){
+                    Text("To save your points please watch this")
+                    HStack(alignment: .center, spacing: 50){
+                        Button("Cancel") {
+                            
+                            presentationMode.wrappedValue.dismiss()
+                            gameSetting.score = 0
+                            
+                        }.padding()
+                            .frame(width: 100, height: 50)
+                            .background{
+                                Color.white.opacity(0.5).clipShape(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 0.5))
+                            }
+                        Button("Ok") {
+                            
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        }.padding()
+                            .frame(width: 100, height: 50)
+                            .background{
+                                Color.white.opacity(0.5).clipShape(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 0.5))
+                            }
+                    }
+                    .padding(.bottom, 100)
+                }
+               )
+              
+               .foregroundColor(.white)
+             //  .blur(radius: 3)
+               .padding(.top, 30)
+              // .background(.ultraThinMaterial)
+          //  Text("reclama")
         }
     }
 }
@@ -21,3 +66,6 @@ struct ReclamaView_Previews: PreviewProvider {
         ReclamaView()
     }
 }
+
+
+
--- Game2022Octomber/Game/Shape.swift
+++ Game2022Octomber/Game/Shape.swift
@@ -56,3 +56,43 @@ struct RectangleTopElipse: Shape {
         }
     }
 }
+
+
+struct qestionShape : Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        return Path { path in
+            
+            let radius = rect.maxX / 16
+            let delta  = rect.maxX / 3
+            
+            path.move(to: CGPoint(x: 0, y: radius ))
+            path.addArc(center: CGPoint(x: radius, y: radius),
+                        radius: radius,
+                        startAngle: Angle(degrees: 180),
+                        endAngle: Angle(degrees: -90),
+                        clockwise: false)
+            path.addLine(to: CGPoint(x: rect.maxX - radius, y: rect.minY))
+            path.addArc(center: CGPoint(x: rect.maxX - radius, y: rect.minY + radius ),
+                        radius: radius,
+                        startAngle: Angle(degrees: -90),
+                        endAngle: Angle(degrees: 0),
+                        clockwise: false)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - radius - delta))
+            path.addArc(center: CGPoint(x: rect.maxX - radius, y: rect.maxY - radius - delta),
+                        radius: radius,
+                        startAngle: Angle(degrees: 0),
+                        endAngle: Angle(degrees: 90),
+                        clockwise: false)
+            path.addLine(to: CGPoint(x: rect.maxX  - delta , y: rect.maxY  - delta))
+            path.addLine(to: CGPoint(x: rect.midX   , y: rect.maxY + delta / 2  - delta))
+            path.addLine(to: CGPoint(x: rect.minX  + delta  , y: rect.maxY  - delta ))
+            path.addArc(center: CGPoint(x: rect.minX + radius, y: rect.maxY - radius - delta),
+                        radius: radius,
+                        startAngle: Angle(degrees: 90),
+                        endAngle: Angle(degrees: 180),
+                        clockwise: false)
+            path.closeSubpath()
+        }
+    }
+}
